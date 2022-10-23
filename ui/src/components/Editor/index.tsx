f2f50f53ed72628da28130144132879d6f1c6017
awxxxxxx <dwaterbear@163.com>
2022-10-23 18:46:10 +0800

    feat: update editor default wording

--- ui/src/components/Editor/index.tsx
+++ ui/src/components/Editor/index.tsx
@@ -96,7 +96,7 @@ export const Editor = (props: IEditorProps) => {
                 id: "-MhwnSs3Dw",
                 type: "header",
                 data: {
-                  text: "What does it mean «block-styled editor»",
+                  text: "What does it mean «Data Editor»",
                   level: 3,
                 },
               },
@@ -104,43 +104,7 @@ export const Editor = (props: IEditorProps) => {
                 id: "Ptb9oEioJn",
                 type: "paragraph",
                 data: {
-                  text: 'Workspace in classic editors is made of a single contenteditable element, used to create different HTML markups. Editor.js <mark class="cdx-marker">workspace consists of separate Blocks: paragraphs, headings, images, lists, quotes, etc</mark>. Each of them is an independent contenteditable element (or more complex structure) provided by Plugin and united by Editor\'s Core.',
-                },
-              },
-              {
-                id: "-J7nt-Ksnw",
-                type: "paragraph",
-                data: {
-                  text: 'There are dozens of <a href="https://github.com/editor-js">ready-to-use Blocks</a> and the <a href="https://editorjs.io/creating-a-block-tool">simple API</a> for creation any Block you need. For example, you can implement Blocks for Tweets, Instagram posts, surveys and polls, CTA-buttons and even games.',
-                },
-              },
-              {
-                id: "SzwhuyoFq6",
-                type: "header",
-                data: {
-                  text: "What does it mean clean data output",
-                  level: 3,
-                },
-              },
-              {
-                id: "x_p-xddPzV",
-                type: "paragraph",
-                data: {
-                  text: "Classic WYSIWYG-editors produce raw HTML-markup with both content data and content appearance. On the contrary, Editor.js outputs JSON object with data of each Block. You can see an example below",
-                },
-              },
-              {
-                id: "6W5e6lkub-",
-                type: "paragraph",
-                data: {
-                  text: 'Given data can be used as you want: render with HTML for <code class="inline-code">Web clients</code>, render natively for <code class="inline-code">mobile apps</code>, create markup for <code class="inline-code">Facebook Instant Articles</code> or <code class="inline-code">Google AMP</code>, generate an <code class="inline-code">audio version</code> and so on.',
-                },
-              },
-              {
-                id: "eD2kuEfvgm",
-                type: "paragraph",
-                data: {
-                  text: "Clean data is useful to sanitize, validate and process on the backend.",
+                  text: 'Workspace in classic editors is made of a single contenteditable element, used to create different HTML markups. Data editor <mark class="cdx-marker">workspace consists of separate Blocks: paragraphs, headings, images, lists, quotes, visualization etc</mark>. Each of them is an independent contenteditable element (or more complex structure) provided by Plugin and united by Editor\'s Core.',
                 },
               },
               {
@@ -152,13 +116,6 @@ export const Editor = (props: IEditorProps) => {
                 id: "N83DHTfUCN",
                 type: "sql",
                 data: {},
-              },
-              {
-                id: "IpKh1dMyC6",
-                type: "paragraph",
-                data: {
-                  text: "We have been working on this project more than three years. Several large media projects help us to test and debug the Editor, to make it's core more stable. At the same time we significantly improved the API. Now, it can be used to create any plugin for any task. Hope you enjoy. 😏",
-                },
               },
             ],
           }
